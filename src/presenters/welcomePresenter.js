60316cc678fc835affad5313fa34d9db6a8959ff
Nils Merkel <nmerkel@kth.se>
2021-08-19 14:05:59 +0200

    first commit after react-create

---
+++ src/presenters/welcomePresenter.js
@@ -0,0 +1,8 @@
+import WelcomeView from "../views/welcomeView";
+
+function WelcomePresenter(props){
+    return(<div className="welcomePresenter">
+        <WelcomeView/>
+    </div>)
+}
+export default WelcomePresenter
---
+++ src/views/welcomeView.js
@@ -0,0 +1,12 @@
+
+function WelcomeView() {
+    return (
+        <div className="WelcomeView">
+            <div className="welcome-titleDiv">
+                <p className="welcome-mainTitle">SVENSKA SCENARION</p>
+                <p className="welcome-subTitle">Spelet - Vad gör du?</p>
+            </div>
+        </div>
+    );
+}
+export default WelcomeView;
